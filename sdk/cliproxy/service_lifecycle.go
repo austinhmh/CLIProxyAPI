5d784c5a401d99288713ebd2d2a2ccf008f3f9e7
austinhmh <2287728420@qq.com>
2026-09-09 11:02:36 +0800

    fix: stop automatic usage probe requests
    Disable the service-started usage refresh prober so synthetic model checks no longer appear as failed request events.

--- sdk/cliproxy/service_lifecycle.go
+++ sdk/cliproxy/service_lifecycle.go
@@ -204,10 +204,6 @@ func (s *Service) Run(ctx context.Context) error {
 		interval := 15 * time.Minute
 		s.coreManager.StartAutoRefresh(context.Background(), interval)
 		log.Infof("core auth auto-refresh started (interval=%s)", interval)
-
-		usageInterval := 5 * time.Minute
-		s.coreManager.StartUsageRefresh(context.Background(), usageInterval)
-		log.Infof("core auth usage-refresh prober started (interval=%s)", usageInterval)
 	}
 
 	select {
